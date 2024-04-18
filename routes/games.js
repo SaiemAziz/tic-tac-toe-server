be3051d59b83e570612d041247520de5ebd9c92d
SaiemAziz <sayemazizchy@gmail.com>
2024-04-18 08:02:45 +0600

    my games added

--- routes/games.js
+++ routes/games.js
@@ -22,5 +22,60 @@ router.post("/insert-game", async (req, res) => {
   const result = await gameCollection?.insertOne(req?.body);
   res.send({ data: result });
 });
+router.get("/my-games", async (req, res) => {
+  const email = req?.query?.email;
+  const sentGames = await gameCollection
+    ?.find({ sender: email })
+    .sort({ time: -1 })
+    .toArray();
+  const recGames = await gameCollection
+    ?.find({ reciever: email })
+    .sort({ time: -1 })
+    .toArray();
+  const users = await userCollection?.find({}).toArray();
+  const modUsers = {};
+  users.forEach((u) => (modUsers[u?.email] = u));
+  const allGames = {};
+  sentGames.forEach((g) => {
+    allGames[g?.reciever] = {
+      opponent: modUsers[g?.reciever],
+      games: Array.isArray(allGames[g?.reciever]?.games)
+        ? [
+            ...allGames[g?.reciever]?.games,
+            {
+              ...g,
+              winner: "p1" ? g?.sender : "p2" ? g?.reciever : "",
+            },
+          ]
+        : [
+            {
+              ...g,
+              winner: "p1" ? g?.sender : "p2" ? g?.reciever : "",
+            },
+          ],
+    };
+  });
+  recGames.forEach((g) => {
+    allGames[g?.sender] = {
+      opponent: modUsers[g?.sender]?.games,
+      games: Array.isArray(allGames[g?.sender]?.games)
+        ? [
+            ...allGames[g?.sender],
+            {
+              ...g,
+              winner: "p1" ? g?.sender : "p2" ? g?.reciever : "",
+            },
+          ]
+        : [
+            {
+              ...g,
+              winner: "p1" ? g?.sender : "p2" ? g?.reciever : "",
+            },
+          ],
+    };
+  });
+
+  res.send({ data: allGames });
+});
 
 module.exports = router;
